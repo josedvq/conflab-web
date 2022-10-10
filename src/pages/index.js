6ef97a47594ed446e1093c73cb1fa6e201f16d08
Jose Vargas <josedvq@gmail.com>
2022-10-10 15:53:53 +0200

    adding hiw section

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,6 +42,39 @@ function HomepageHeader() {
   );
 }
 
+const hiw_list = [
+  {
+    title: '1. Sign up',
+    Svg: require('@site/static/img/signup.svg').default,
+    description: (
+      <p>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLScLYMdZ7YRzHNMAJRUMY9YkurOUJ21jHjJ3F4-4CbTLiHOPAw/viewform?usp=sf_link" target="_blank">Sign up</a> for ConfLab by filling in our <a href="https://docs.google.com/forms/d/e/1FAIpQLScLYMdZ7YRzHNMAJRUMY9YkurOUJ21jHjJ3F4-4CbTLiHOPAw/viewform?usp=sf_link" target="_blank">Informed Consent Form</a> where you agree to take part and to donating your data for research purposes. Still not sure? Read our <a href="faq">FAQ</a></p>
+      ),
+
+  },
+  {
+    title: '2. Meet the Chairs on Thursday',
+    Svg: require('@site/static/img/meet.svg').default,
+    description: (
+      <p>Our <a href="#data_collection">data collection</a> will take place on Thursday from 16:30 to 17:30 at the Rhodes hall. After you arrive, we will check if you agreed to <a href="#data">your data</a> for research. You fill in a short survey about your research interests and experience with MM. We will give you our newly designed MINGLE Midge to be worn around your neck. After that you are free to meet peers and the conference's organizational chairs at this event.</p>
+    ),
+  },
+  {
+    title: '3. Tutorial and Debrief on Friday',
+    Svg: require('@site/static/img/tutorial.svg').default,
+    description: (
+      <p>Join us in learning more about the science and technology behind ConfLab including discussions on privacy, ethics, &amp; data sharing.</p>
+    ),
+  },
+  {
+    title: '4. Research and the Future ',
+    Svg: require('@site/static/img/research.svg').default,
+    description: (
+      <p>Your data will help progress research on social interaction analysis in the wild. It will be shared in a pseudonymised form with the research community under an <a href="https://conflab.ewi.tudelft.nl/wp-content/uploads/2019/10/eula.pdf" target="_blank" style="">End User License Agreement</a> to be only used for non-commercial and non-governmental research. We also hope to use it for setting future grand challenges.</p>
+    ),
+  },
+];
+
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
@@ -50,13 +83,27 @@ export default function Home() {
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
+
+        <section className={styles.features}>
+          <div className="container">
+            <div className="row">
+              {FeatureList.map((props, idx) => (
+                <Feature key={idx} {...props} />
+              ))}
+            </div>
+          </div>
+        </section>
         <h1>How it Works</h1>
         <div className={clsx(styles.hiw)}>
+          {hiw_list.map(el => {
+            return <div>
+              <h3>{el.title}</h3>
+              <p>{el.description}</p>
+              
+            </div>
+          })}
           <div></div>
-          <div>
-            <h3>Sign up</h3>
-            <p>Sign up for ConfLab by filling in our Informed Consent Form, where you agree to take part and to donating your data for research purposes.  Still not sure? Read our FAQ.</p>
-          </div>
+          
 
           
         </div>
